b51118f6df0ba4f17cbab710771fff7c6b7b7dee
kazumaneet <mekbibtare@gmail.com>
2023-10-06 11:58:13 +0300

    Implement Tab Switching

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@ import createHeader from "./header";
 import createMainPage from "./mainPage";
 import createContactPage from "./contact";
 import createAboutPage from "./about";
+
 createHeader();
 createMainPage();
+
+var links = document.querySelectorAll('button');
+links.forEach(link => {
+  link.addEventListener('click',createPage)
+})
+
+
+function createPage(e){
+  var pageName = e.target.textContent;
+  var main = document.querySelector('main'); 
+  main.textContent = '';
+  if (pageName === 'main')
+    createMainPage();
+  else if(pageName == 'contact')
+    createContactPage();
+  else
+    createAboutPage();
+}
